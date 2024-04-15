fa0e2a1aa0d0d58e822eba43a8707f7188f3f534
eldin001 <156930112+eldin001@users.noreply.github.com>
2024-04-15 13:52:57 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1 class="text-center">test</h1>
+    <i class="fa fa-solid fa-home"></i>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
