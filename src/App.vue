a65bff6838f16783e9cfc7193551586e62db41f6
eldin001 <eldin.balje@icloud.com>
2024-04-15 15:15:32 +0200

    added store file js

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <h1>{{ store.prova }}</h1>
     <HeaderComponent />
     <MainComponent />
   </div>
@@ -7,9 +8,15 @@
 
 <script>
 import HeaderComponent from './components/HeaderComponent.vue';
-import MainComponent from './components/MainComponent.vue'
+import MainComponent from './components/MainComponent.vue';
+import {store} from './store.js'
   export default {
     name: 'App',
+    data() {
+      return {
+        store
+      }
+    },
     components: {
       HeaderComponent,
       MainComponent,
@@ -17,6 +24,7 @@ import MainComponent from './components/MainComponent.vue'
 
   }
 </script>
+
 
 <style lang="scss" scoped>
 
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,25 +1,44 @@
 <template>
-   <main>
-
-   </main>
+    <main>
+        <select class="ms-5 mt-3 mb-3 " name="" id="">
+            <option value="">Alien</option>
+        </select>
+        <div class="container">
+            <div class="container p-3 d-bug">
+                <div class="found">
+                    Found 30 cards
+                </div>
+                <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex libero tempora quam. Nulla earum necessitatibus consequatur eaque dolorem, eum magni sequi aliquam quaerat mollitia labore illo, unde, consectetur voluptas aperiam.</div>
+            </div>
+        </div>
+    </main>
 </template>
 
 <script>
-    export default {
-        name: 'MainComponent'
-    }
+export default {
+    name: 'MainComponent'
+}
 </script>
 
 <style lang="scss">
 @use '../assets/styles/partials/variables' as *;
 
+.container {
+    background-color: white;
+}
 
+.found {
+    background-color: $lightGray;
+    color: white;
+    padding: 3px;
+}
 
-
-main {
-    height: calc(100vh - 100px);
-    background-color: $orange;
+.d-bug {
+    border: 2px solid red
 }
 
 
+main {
+    background-color: $orange;
+}
 </style>
